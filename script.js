c24fee98705c501c1499c2cfd7408a4422bfad03
miguelitos <aaaolacheese@gmail.com>
2026-07-30 16:19:06 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('meuBotao').addEventListener('click', () => {
+  const mensagem = document.getElementById('mensagem');
+  mensagem.textContent = '🎉 Funciona! O JavaScript está a correr no GitHub Pages.';
+});
